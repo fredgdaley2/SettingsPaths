033229adc9601918a1657f2165b7fa89fdc19e49
Fred Daley <fredgd2@outlook.com>
2022-04-10 15:20:17 -0400

    Initial code commit

---
+++ SettingsPaths/Features/ConfigPaths/ConfigPaths.cs
@@ -0,0 +1,7 @@
+﻿namespace Features;
+internal class ConfigPaths
+{
+    public string Path { get; set; } = String.Empty;
+    public string Value { get; set; } = String.Empty;
+
+}
---
+++ SettingsPaths/Features/ConfigPaths/ConfigPathsInteractions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+namespace Features;
+internal static class ConfigPathsInteractions
+{
+    public static List<ConfigPaths> GetConfigPathsFromAppSettings(string appSettingsPath)
+    {
+        var builder = new ConfigurationBuilder().AddJsonFile(appSettingsPath).Build();
+        var configElements = builder.GetChildren();
+        var cnfgPaths = new List<ConfigPaths>();
+        GetAllChildrenPaths(configElements, cnfgPaths);
+        return cnfgPaths;
+    }
+
+    public static void GetAllChildrenPaths(IEnumerable<IConfigurationSection> sections, List<ConfigPaths> cnfgPaths)
+    {
+        foreach (var child in sections)
+        {
+            if (child.Value != null)
+            {
+                cnfgPaths.Add(new ConfigPaths { Path = child.Path, Value = child.Value });
+            }
+            GetAllChildrenPaths(child.GetChildren(), cnfgPaths);
+        }
+    }
+
+}
